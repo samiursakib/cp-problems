47034694eb5466c559d6c346f61b8b63a2bf1ffa
samiursakib <rafisamiur@gmail.com>
2025-08-29 03:37:25 +0600

    initialized empty project

---
+++ rust/target/debug/deps/rust-937e305e8b55a7b7.d
@@ -0,0 +1,5 @@
+/mnt/extra/cp-problems/rust/target/debug/deps/librust-937e305e8b55a7b7.rmeta: src/main.rs
+
+/mnt/extra/cp-problems/rust/target/debug/deps/rust-937e305e8b55a7b7.d: src/main.rs
+
+src/main.rs:
